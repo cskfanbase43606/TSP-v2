dd08ca2a235b0c53863c2a7b9fbc41c9e55f0fac
CSKFanbase07 <102754810+cskfanbase43606@users.noreply.github.com>
2025-04-18 18:51:35 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -21,33 +21,33 @@ export function Footer() {
       </div>
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/codevinayak"
+          href="https://www.linkedin.com/in/rahul-rana-1b794b245/?originalSubdomain=in"
           target="_blank"
           rel="noreferrer"
         >
           <img src={linkedin} alt="Linkedin" />
         </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/cskfanbase43606"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
-        <a
+{/*         <a
           href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
           target="_blank"
           rel="noreferrer"
         >
           <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
+        </a> */}
+{/*         <a
           href="https://t.me/CodeVinayak"
           target="_blank"
           rel="noreferrer"
         >
           <img src={telegram} alt="telegram" />
-        </a>
+        </a> */}
         <a
           href="https://www.instagram.com/vinayaksingh.in"
           target="_blank"
